b17320e28e558dcc957f49cc71ac534286982717
Sukant Hajra <rrudbskr5g@snkmail.com>
2017-02-12 11:28:20 -0600

    not a Rails project anymore; just a static site

--- README.rst
+++
@@ -1,5 +0,0 @@
-Source Code for Little Rascals Pet Service
-==========================================
-
-This is the source code for a pet service business at
-http://littlerascalsaustin.com.
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-LittlerascalsSite::Application.config.secret_token = '805213b1f8664ced2c46447af4ef26c9fe665c173762bd6ec2d9af66b0c7c1082cd0bdbb929e88b742346be7f1a25797b4a071a74940df7a194033c08c0461ee'
